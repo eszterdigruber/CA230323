93a5db97b2f049dac5db8e3abee1a83abe4cba56
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-23 14:00:03 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-﻿namespace HelloWorld
+﻿using System.IO;
+using System.Text;
+
+namespace HelloWorld
 {
+    static List<Felfedezesek> Felfed_List = new List<Felfedezesek()>;
     class Program
     {
         static void Main(string[] args)
