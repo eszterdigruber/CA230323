56500d69c4d03674f7c3b28b9f73a84b28d59b50
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-23 14:04:09 +0100

    asd

--- Felfedezesek.cs
+++ Felfedezesek.cs
@@ -2,6 +2,21 @@
 {
     internal class Felfedezesek
     {
+        //Év;Elem;Vegyjel;Rendszám;Felfedező
+        public string Ev;
+        public string Elem;
+        public string Vegyjel;
+        public int Rendszam;
+        public string Felfedezo;
 
+        public Felfedezesek(string Sor)
+        {
+            var dbok = Sor.Split(';');
+            this.Ev = dbok[0];
+            this.Elem = dbok[1];
+            this.Vegyjel = dbok[2];
+            this.Rendszam = int.Parse(dbok[3]);
+            this.Felfedezo = dbok[4];
+        }
     }
 }
