6e590fa12b889d413e86974ed225820cd34307ab
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-23 14:08:17 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -3,12 +3,19 @@ using System.Text;
 
 namespace HelloWorld
 {
-    static List<Felfedezesek> Felfed_List = new List<Felfedezesek()>;
     class Program
     {
+        static List<Felfedezesek> Felfed_List = new List<Felfedezesek>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Feladat4(); Console.WriteLine("\n-------------------------------\n");
+        }
+
+        private static void Feladat4()
+        {
+            Console.WriteLine("4.feladat: Beolvasás");
+            var sr = new StreamReader(@"felfedezesek.csv", Encoding.UTF8);
+
         }
     }
 }
