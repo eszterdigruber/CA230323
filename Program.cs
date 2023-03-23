3d4ce8d028bd6809ca012f00916ed81ff63ff759
Eszter Digruber <digruber.eszter@verebelyszki.hu>
2023-03-23 14:30:01 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -6,9 +6,35 @@ namespace HelloWorld
     class Program
     {
         static List<Felfedezesek> Felfed_List = new List<Felfedezesek>();
+        static string KeresVegyjel;
         static void Main(string[] args)
         {
             Feladat4(); Console.WriteLine("\n-------------------------------\n");
+            Feladat6(); Console.WriteLine("\n-------------------------------\n");
+            Feladat7(); Console.WriteLine("\n-------------------------------\n");
+        }
+
+        private static void Feladat7()
+        {
+            Console.WriteLine("7.feladat: Bekérés");
+            Console.Write("Kérem adjon meg egy vegyjelet: ");
+            KeresVegyjel = Console.ReadLine();
+
+
+        }
+
+        private static void Feladat6()
+        {
+            Console.WriteLine("6.feladat: William Ramsay");
+            int darab = 0;
+            foreach (var f in Felfed_List)
+            {
+                if (f.Felfedezo.Replace(" ", "").Contains("WilliamRamsay") || f.Felfedezo.Replace(" ", "").Contains("W.Ramsay"))
+                {
+                    darab++;
+                }
+            }
+            Console.WriteLine($"Ennyi esetben dolgozott William Ramsay együtt: {darab}");
         }
 
         private static void Feladat4()
